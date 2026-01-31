58b33ec8cd4f6275786701d1a083457622a2ffba
tareqferdous <16101017@uap-bd.edu>
2026-01-31 21:11:21 +0600

    all provider and provider details api integrated

---
+++ src/service/provider.service.ts
@@ -0,0 +1,32 @@
+const API_URL = process.env.API_URL;
+
+const getAllProviders = async () => {
+  try {
+    const res = await fetch(`${API_URL}/providers`);
+    const data = await res.json();
+    if (!data.success) {
+      throw new Error("Failed to fetch providers");
+    }
+    return { data: data, error: null };
+  } catch (error) {
+    return { data: null, error: error };
+  }
+};
+
+const getProviderById = async (id: string) => {
+  try {
+    const res = await fetch(`${API_URL}/providers/${id}`);
+    const data = await res.json();
+    if (!data.success) {
+      throw new Error("Failed to fetch provider");
+    }
+    return { data: data, error: null };
+  } catch (error) {
+    return { data: null, error: error };
+  }
+};
+
+export const providerService = {
+  getAllProviders,
+  getProviderById,
+};
